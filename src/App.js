7d8dab6c386d2c78233d368b411357d6c45d69d6
shin101 <shinj0127@gmail.com>
2023-05-01 16:05:04 -0700

    set up frontend router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+
   return (
     <div>
-     
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" exact element={<Home/>} />
+            <Route path="/login" exact element={<Login/>} />
+            <Route path="*" element={<NotFound/>} />
+            {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+    );
 }
 
 export default App;
