43e75b25cae79bacc6e9171e993d9ad2cac32036
shin101 <shinj0127@gmail.com>
2023-05-25 12:22:56 -0700

    Added Apollo Client

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { useReactiveVar } from "@apollo/client";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, darkTheme, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
@@ -18,20 +18,22 @@ function App() {
 
   return (
     <div>
-      <HelmetProvider>
-        <BrowserRouter>
-          <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-            <GlobalStyles />
-              <Routes>
-                  <Route exact path={routes.home} element={isLoggedIn? <Home /> : <Login />} /> 
-                  <Route exact path="/login" element={<Login/>} />
-                    {!isLoggedIn && <Route exact path="/sign-up" element={<SignUp/>} /> }
-                  <Route exact path="*" element={<NotFound/>} />
-                  {/* Alternatively, useRoute path="*" element={<Navigate to="/" />} /> */}
-              </Routes>
-          </ThemeProvider>
-        </BrowserRouter>
-      </HelmetProvider> 
+      <ApolloProvider client={client}>
+        <HelmetProvider>
+          <BrowserRouter>
+            <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+              <GlobalStyles />
+                <Routes>
+                    <Route exact path={routes.home} element={isLoggedIn? <Home /> : <Login />} /> 
+                    <Route exact path="/login" element={<Login/>} />
+                      {!isLoggedIn && <Route exact path="/sign-up" element={<SignUp/>} /> }
+                    <Route exact path="*" element={<NotFound/>} />
+                    {/* Alternatively, useRoute path="*" element={<Navigate to="/" />} /> */}
+                </Routes>
+            </ThemeProvider>
+          </BrowserRouter>
+        </HelmetProvider> 
+      </ApolloProvider>
     </div>
     );
     
