89170bcc9568b76cb6e240a3af5f24a6c382f4ec
shin101 <shinj0127@gmail.com>
2023-06-19 21:47:19 +0900

    create account - register page works

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCameraRetro } from '@fortawesome/free-solid-svg-icons'
+import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 import routes from "./routes";
 import AuthLayout from "../components/auth/AuthLayout";
 import Button from "../components/auth/Button";
@@ -9,41 +9,113 @@ import BottomBox from "../components/auth/BottomBox";
 import styled from "styled-components";
 import { FatLink } from "../components/shared";
 import PageTitle from "../components/PageTitle";
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
 const HeaderContainer = styled.div`
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const SignupText = styled(FatLink)`
   font-size: 16px;
   text-align;: center;
   margin-top: 10px;
-`
+`;
 
-function SignUp(){
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
+function SignUp() {
+  const navigate = useNavigate();
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    navigate(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+
+    createAccount({ variables: { ...data } });
+  };
   return (
-    <AuthLayout>  
+    <AuthLayout>
       <PageTitle title="Sign Up" />
-        <FormBox>
+      <FormBox>
         <HeaderContainer>
           <FontAwesomeIcon icon={faCameraRetro} size="3x" />
         </HeaderContainer>
-          <SignupText>Sign up to see photos and videos from your friends</SignupText>
-          <form>
-            <Input type="text" placeholder="Email" />
-            <Input type="text" placeholder="Name" />
-            <Input type="text" placeholder="Username" />
-            <Input type="password" placeholder="Password" />
-            <Button type="submit" value="Sign up" />
-          </form>
-        
-        </FormBox>
-        <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
+        <SignupText>
+          Sign up to see photos and videos from your friends
+        </SignupText>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", { required: "First name is required" })}
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("username", { required: "Username is required" })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register("email", { required: "Email is required" })}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            {...register("password", { required: "Password is required" })}
+            type="password"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign Up"}
+            disabled={!formState.isValid || loading}
+          />
+        </form>
+      </FormBox>
+      <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
     </AuthLayout>
   );
 }
-
 
 export default SignUp;
