3f33ec150248b59cd17ea7b266dc06d67d5a5635
shin101 <shinj0127@gmail.com>
2023-06-23 14:26:18 +0900

    Photo component part one

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,58 @@
 import { useNavigate } from "react-router-dom";
 import { LogUserOut } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Avatar from "../components/Avatar";
+import { FatText } from "../components/shared";
 
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
 
 function Home() {
+  const { data } = useQuery(FEED_QUERY);
+
   const navigate = useNavigate();
 
   return (
     <div>
-      this is home page
-      <button onClick={() => LogUserOut(navigate)}>Log Out</button>
+      {data?.seeFeed?.map((photo) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 }
